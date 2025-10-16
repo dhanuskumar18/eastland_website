628fcec36a8febd2feeeaabcba5504b5db97ed91
dhanuskumar <dhanuskumar18@gmail.com>
2025-10-16 17:45:55 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Eastland Website',
+  description: 'Basic Next.js + Tailwind starter',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-dvh antialiased">
+        {children}
+      </body>
+    </html>
+  )
+}
