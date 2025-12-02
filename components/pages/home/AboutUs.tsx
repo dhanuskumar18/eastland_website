9bffca328d72716a8d9f7a038e1dc326fbeb3fa6
dhanuskumar <dhanuskumar18@gmail.com>
2025-12-02 14:42:20 +0530

    fixed

--- components/pages/home/AboutUs.tsx
+++ components/pages/home/AboutUs.tsx
@@ -122,14 +122,13 @@ export default function AboutUs({ content, sectionId }: AboutUsProps = {}) {
               <svg
                 width="14"
                 height="14"
-                className="sm:w-4 sm:h-4"
+                className="sm:w-4 sm:h-4 transition-transform duration-300 rotate-0 group-hover:rotate-45"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="transition-transform duration-300 rotate-0 group-hover:rotate-45"
               >
                 <path d="M7 17L17 7M17 7H7M17 7V17"/>
               </svg>
